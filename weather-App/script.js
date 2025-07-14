cd1a00ef7d9344378b05d339654133d391cd6d1d
Gopal-G0 <heyitsmegopal@gmail.com>
2025-07-14 14:32:24 +0530

    edited some css and design

--- weather-App/script.js
+++ weather-App/script.js
@@ -1,3 +1,11 @@
+const api_key = 'f8bb4d2ea0a07e8c2696cb19294850eb';
+const base_url = `https://api.openweathermap.org/data/2.5/weather?q=Noida&appid=${api_key}`;
 
+async function getWeather() {
+    const response = await fetch(base_url);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.main.feels_like);
+}
 
-
+getWeather();
